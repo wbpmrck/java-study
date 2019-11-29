907c84b2318c8ae1f4bff8af85b648919f66ddcf
kaicui <wbpmrck2003@gmail.com>
2019-11-29 16:17:45 +0800

    (remove unused imports)

--- spring-in-action-4-study-case/src/test/java/com/springaction/concert/ConcertTest.java
+++ spring-in-action-4-study-case/src/test/java/com/springaction/concert/ConcertTest.java
@@ -1,17 +1,10 @@
 package com.springaction.concert;
 
-import com.springaction.soundsystem.CDPlayerConfig;
-import com.springaction.soundsystem.CompactDisc;
-import com.springaction.soundsystem.SgtPeppers2;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import static junit.framework.TestCase.assertEquals;
-import static junit.framework.TestCase.assertNotNull;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = JavaConfig.class)
